8720cb91bb8448e320bc2880ed208a3c61308544
mcsarmend <42789413+mcsarmend@users.noreply.github.com>
2021-10-14 17:35:18 -0500

    e agregan cursos

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Slider from "./components/slider/Slider";
 import Info from "./components/info/Info";
 import Footer from "./components/footer/Footer";
 import Techs from "./components/Techs/Techs"
+import Training from "./components/training/Training"
 function App() {
   const [scrollHeight, setScrollHeight] = useState(0);
 
@@ -25,6 +26,7 @@ function App() {
       <Cover />
       <About />
       <Techs />
+      <Training />
       <Slider />
       <Info />
       <Footer />
