d58d53e8cc96a4012917668ff286bd7380c78f3d
mcsarmend <42789413+mcsarmend@users.noreply.github.com>
2021-10-20 18:16:49 -0500

    Se quita llamado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter,Router, Route, Switch, Redirect } from "react-router-dom";
-import Error404 from "./components/Error404/Error404";
 
 ReactDOM.render(
   <React.StrictMode>
